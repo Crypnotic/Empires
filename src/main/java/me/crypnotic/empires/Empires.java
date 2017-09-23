4f43f87684dae2f9e288975d3e1c671d167b4dc6
Crypnotic <crypnoticofficial@gmail.com>
2017-09-23 18:41:45 -0500

    Create main file and plugin.yml

---
+++ src/main/java/me/crypnotic/empires/Empires.java
@@ -0,0 +1,21 @@
+package me.crypnotic.empires;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Empires extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+}
